c0ad947ee67321a60333bdb279173918d1969da8
sahik10 <67615603+sahik10@users.noreply.github.com>
2020-10-01 12:27:14 +0530

    Create Spiral.py

---
+++ Spiral.py
@@ -0,0 +1,18 @@
+import turtle
+
+turtle.speed(0)
+turtle.bgcolor("black")
+
+for i in range(10) :
+        for colours in ["red", "magenta", "blue", "cyan", "yellow", "white"] :
+                turtle.color(colours)
+                turtle.pensize(3)
+                turtle.left(12)
+                turtle.forward(200)
+                turtle.left(90)
+                turtle.forward(200)
+                turtle.left(90)
+                turtle.forward(200)
+                turtle.left(90)
+                turtle.forward(200)
+                turtle.left(90)
